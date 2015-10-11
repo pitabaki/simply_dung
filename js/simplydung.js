01ddac12eb46c70c9172dd649f63364943863820
Peter Berki <pberki1986@gmail.com>
2015-10-10 22:55:16 -0700

    Portfolio filter available. JavaScript notation included for posterity.

--- js/simplydung.js
+++ js/simplydung.js
@@ -24,103 +24,72 @@ function init(){
 	//empty array, to push classNames minus selected ID
 	var idQuery = [];
 
-	//create a comparable piece of data (i.e. an array of the data-category data)
-	for(var q = 0; q < classNames.length; q++){
-		classQuery.push("[data-category=" + "'" + classNames[q] + "']");
-	}
-
 	//class that gets added when a button is selected. Class is added to images
 	var activeClass = " active";
 
-	var itemQuery = document.querySelectorAll("[data-category]");
+	//Included only for testing purposes
+
+	/* var itemQuery = document.querySelectorAll("[data-category='marketing']");
+	console.log(itemQuery);
+	console.log (itemQuery.length); */
 
 	//What happens when a button is clicked follows
 	$("button").click(function(){
 
-		//active class needs to be removed first
-		//removes the class "active" attached to images
-		$("img").removeClass("active");
-
-		//removes the class "active" attached to buttons
-		$("button").removeClass("active");
-
 		//When button is clicked, an ID is returned
 		var id = this.id;
-		var button = document.getElementById(id);
-		button.className = activeClass;
 
-		//The code silenced out is a reminder. If I decide to remove jQuery implementation, the following will remind me where I left off.
-		/*
-		var portfolio_section = document.getElementById("portfolio");
-		var general_data = portfolio_section.getElementsByClassName("active");
-		if(general_data.length > 0){
-			for(var z = 0; z < general_data.length; z++){
-				general_data[z].className -= "active";
-				console.log(general_data.length);
-			}
-		}*/
+		//query arrays and selection array need to be cleared when the button is clicked
+		idQuery = [];
+		classQuery = [];
+		var selection = [];
 
-		/*var remainCat = document.querySelectorAll(classQuery);
-		console.log("remainCat length: " + remainCat.length);*/
+		//active class needs to be removed first
+		//removes the class "active" attached to images
+		$("img").removeClass(activeClass);
 
+		//removes the class "active" attached to buttons
+		$("button").removeClass(activeClass);
 
-/*
-		//cycle through classNames
-		//once a button is pressed, the id is sent through the following for-loop to find a match
-		for(var i = 0; i < classNames.length;i++){
+		if(id !== "clear"){
+			//button (id) is selected and the class (defined globally), "active", is attached to button
+			var button = document.getElementById(id);
+			button.className = activeClass;
 
-
-
-			//if the selected id matches one of the categories in the className array, then...
-			if(id === classNames[i]){
-
-				//so that the id is used as the category query, the following needs to be concatenated
-				//create a variable to store concatenation
-				var data_categories = "[data-category=" + "'" + id + "']";
-
-				//search for that particular data attribute
-				var selection = document.querySelectorAll(data_categories);
-
-				for(var z = 0;z < remainCat.length; z++){
-					if(remainCat[z] !== selection){
-
-						remainCat[z].className += activeClass;
-						//apply event to each match
-						//in order to apply an event to each selection, another for-loop is constructed
-						//this for-loop works off of how many returns were made from the query
-						//for(var g = 0; g < empty; g++){
-							//selection[g].className += activeClass;
-							//console.log(selection[g]);
-						//}
-					}
+			//i is less than class names so for loop will only loop through the amount of names in classNames
+			//the reason is because we only need to log one data-category for identification purposes
+			//idQuery stores data-category (data attribute)
+			for(var i = 0; i < classNames.length; i++){
+				if(id !== classNames[i]){
+					idQuery.push("[data-category=" + "'" + classNames[i] + "']");
 				}
 			}
-		}*/
 
-
-
-
-		//cycle through classNames
-		//once a button is pressed, the id is crossreferenced with the classNames array
-		for(var i = 0; i < classNames.length;i++){
-
-			//for every category attribute that doesn't match the ID, the following happens
-			if(id !== classNames[i]){
-
-				//array idQuery (defined above) is updated with the list of attributes that didn't match the ID (button pushed)
-				idQuery.push("[data-category=" + "'" + classNames[i] + "']");
+			//search for that particular data attribute and push he results into selection
+			//this returns the html element
+			for(var qr = 0; qr < idQuery.length; qr++ ){
+				selection.push(document.querySelectorAll(idQuery[qr]));
 			}
-		}
- 		var selection = [];
-		//search for that particular data attribute
-		for(var qr = 0; qr < itemQuery.length; qr++ ){
-			selection.push(document.querySelectorAll(idQuery[qr]));
-		}
-		for(var t = 0; t < selection.length; t++){
-			selection[t].className += activeClass;
-		}
-		console.log(selection.length);
+			//console.log(selection);
 
+			//loop through returned data and attach an activeClass
+			//start loop with the length of selection
+			for(i = 0; i < selection.length; i++){
+				//if(selection[i].length >= 0){
+
+					//start a second loop to select each, individual img
+					//without this second loop, the active class is attached to the general selection
+					//example: if the dataset is marketing, activeClass would get attached to marketing as a whole
+					//but we want each image in marking to have activeClass
+					for(var g = 0; g < selection[i].length; g++){
+						selection[i][g].className = activeClass;
+					}
+				//}
+			}
+			return false;
+		}else{
+			return false;
+		}
 	});
 }
 window.onload = init;
